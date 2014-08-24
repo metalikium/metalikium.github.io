05c26336d2f857141f3393f423c45067b80f9e76
metalikium <metalikium@users.noreply.github.com>
2014-08-24 13:50:08 +0200

    Add page transitions with jquery.smoothState

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+(function ($) {
+  'use strict';
 
+  var $body = $('html, body'), // Define jQuery collection 
+
+  content  = $('#page').smoothState({
+    onStart : {
+    duration: 250,
+    render: function () {
+        content.toggleAnimationClass('is-exiting');
+        // Scroll user to the top
+        $body.animate({ 'scrollTop': 0 });
+      }
+    }
+  }).data('smoothState');
+
+})(jQuery);
